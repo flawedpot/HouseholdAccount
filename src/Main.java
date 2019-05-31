ae6b1ae56479d9ead3434c11ca327aab50f840f4
flawedpot <ryu.bo.bora.boragera.1352@gmail.com>
2019-05-31 21:59:36 +0900

    Mainクラスを変更する#1　データを生成し、内容を表示する

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		int i = 4;
 
+		while(i != 0) {
+			System.out.println("実行したい操作を選択してください：");
+			System.out.println("1：入力  2:一覧を表示する  3:結果を出力する 0:終了する");
+			Scanner sc = new Scanner(System.in);
+			i = sc.nextInt();
+			switch (i) {
+			case 1:
+				Bookdata data = new Bookdata();
+				data.AddBookdata();
+				System.out.println(data);
+				break;
+			case 2:
+				/* 後でつくる */
+				break;
+			case 3:
+				/* 後でつくる */
+				break;
+			default:
+				System.out.println("プログラムを終了します");
+			}
+		}
 	}
 }
